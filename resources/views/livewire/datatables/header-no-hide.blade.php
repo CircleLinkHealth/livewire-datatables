49f1b3feae7dbcc9c6e83a03e57416315875b264
kountouris7 <kountouris7@gmail.com>
2021-11-14 14:56:25 +0200

    [WIP] sort direction indication icons for multisorting

--- resources/views/livewire/datatables/header-no-hide.blade.php
+++ resources/views/livewire/datatables/header-no-hide.blade.php
@@ -11,7 +11,7 @@
                     class="w-full h-full px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider flex items-center focus:outline-none @if($column['align'] === 'right') justify-end @elseif($column['align'] === 'center') justify-center @endif">
                 <span class="inline ">{{ str_replace('_', ' ', $column['label']) }}</span>
                 <span class="inline text-xs text-blue-400">
-                     @if($this->multisortable === false)
+          @if($this->multisortable === false)
                         @if(isset($sort[0]) && $this->getIndexFromValue($sort[0]) == $index && ($direction = $this->getColumnDirection($sort[0])))
                             @if($direction === 'asc')
                                 <x-icons.chevron-up wire:loading.remove class="h-6 w-6 text-green-600 stroke-current"/>
@@ -21,7 +21,16 @@
                             @endif
                         @endif
                     @else
-{{--                         wip--}}
+                        @foreach($sort as $value)
+                            @if($this->getIndexFromValue($value) == $index && ($direction = $this->getColumnDirection($value)))
+                                @if($direction === 'asc')
+                                    <x-icons.chevron-up wire:loading.remove class="h-6 w-6 text-green-600 stroke-current"/>
+                                @endif
+                                @if($direction === 'desc')
+                                    <x-icons.chevron-down wire:loading.remove class="h-6 w-6 text-green-600 stroke-current"/>
+                                @endif
+                            @endif
+                        @endforeach
                     @endif
             </span>
             </button>
