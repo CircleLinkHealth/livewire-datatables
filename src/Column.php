7b6d73835a87cf9396c5e81f692f1f14b71e3060
kountouris7 <kountouris7@gmail.com>
2021-11-17 14:12:11 +0200

    [WIP] last $direction fixed on defaultDirection in Column.php

--- src/Column.php
+++ src/Column.php
@@ -120,7 +120,7 @@ class Column
         return $this;
     }
 
-    public function defaultSort($direction = true)
+    public function defaultSort($direction = 'desc')
     {
         $this->defaultSort = $direction;
 
